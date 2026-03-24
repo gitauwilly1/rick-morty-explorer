b1bcf2ebfa9ff52b019af11897af6c7c723439b6
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-03-24 14:38:20 +0300

    Add navigation bar with routing links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="bg-gray-900 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">Rick & Morty Explorer</h1>
+        <div className="space-x-4">
+          <Link 
+            to="/dashboard" 
+            className={({ isActive }) => 
+              `hover:text-blue-300 transition ${
+                isActive ? 'text-blue-300' : ''
+              }`
+            }
+            end
+          >
+            Home
+          </Link>
+          <Link 
+            to="/dashboard/characters" 
+            className={({ isActive }) => 
+              `hover:text-blue-300 transition ${
+                isActive ? 'text-blue-300' : ''
+              }`
+            }
+          >
+            Characters
+          </Link>
+          <Link 
+            to="/dashboard/about" 
+            className={({ isActive }) => 
+              `hover:text-blue-300 transition ${
+                isActive ? 'text-blue-300' : ''
+              }`
+            }
+          >
+            About
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
