fbdc81a9324aeefacf77b412d622e698e73ec27e
William Gitau <162879882+gitauwilly1@users.noreply.github.com>
2026-03-24 14:37:11 +0300

    Update footer with branding and copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-gray-100 border-t border-gray-200 p-4 text-center text-gray-600">
+      <p>Rick & Morty Explorer - Created with React Router</p>
+      <p className="text-sm mt-1">Data provided by Rick and Morty API</p>
+      <p className="text-xs mt-2">© {currentYear} All rights reserved</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
